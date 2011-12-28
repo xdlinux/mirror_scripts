dd6bd54d41c09ed8eb38eca566c3e5887647dca2
xdlinux <xdosc.ibmclub@gmail.com>
2011-12-28 13:30:25 +0800

    new script for slackware

--- scripts/slackware.sh
+++ scripts/slackware.sh
@@ -7,15 +7,17 @@
 # and Roman Kyrylych <roman@archlinux.org>
 # Licensed under the GNU GPL (version 2)
 
+source `dirname $0`/functions.d/functions
 # Filesystem locations for the sync operations
-SYNC_HOME="/home/bigeagle/mirror"
 SYNC_LOGS="$SYNC_HOME/logs/slackware"
 SYNC_FILES="/srv/ftp/slackware/slackware-13.37/"
 SYNC_LOCK="$SYNC_HOME/slackware.lck"
 #SYNC_SERVER="rsync://mirror6.bjtu.edu.cn/slackware/slackware-13.1/"
 #SYNC_SERVER="rsync://mirrors6.ustc.edu.cn/slackware/slackware-13.1/"
-SYNC_SERVER="rsync://mirror.yandex.ru/slackware/slackware-13.37/"
+#SYNC_SERVER="rsync://mirror.yandex.ru/slackware/slackware-13.37/"
+SYNC_SERVER="rsync://ftp.heanet.ie/pub/slackware/pub/slackware/slackware-13.37"
 LOG_FILE="slackware_$(date +%Y%m%d-%H).log"
+STAT_FILE="$SYNC_HOME/status/slackware"
 
 # Do not edit the following lines, they protect the sync from running more than
 # one instance at a time
@@ -28,6 +30,7 @@ fi
 touch "$SYNC_LOCK"
 # End of non-editable lines
 
+set_stat $STAT_FILE "-1"
 # Create the log file and insert a timestamp
 touch "$SYNC_LOGS/$LOG_FILE"
 echo "=============================================" >> "$SYNC_LOGS/$LOG_FILE"
@@ -37,11 +40,15 @@ echo ">> ---" >> "$SYNC_LOGS/$LOG_FILE"
 
 rsync -6 -avz --delete-after --exclude *.~tmp~*  $SYNC_SERVER $SYNC_FILES >> $SYNC_LOGS/$LOG_FILE
 
+set_stat $STAT_FILE $?
+
 # Insert another timestamp and close the log file
 echo ">> ---" >> "$SYNC_LOGS/$LOG_FILE"
 echo ">> Finished sync on $(date --rfc-3339=seconds)" >> "$SYNC_LOGS/$LOG_FILE"
 echo "=============================================" >> "$SYNC_LOGS/$LOG_FILE"
 echo "" >> "$SYNC_LOGS/$LOG_FILE"
+
+date --rfc-3339=seconds > "$SYNC_FILES/lastsync"
 
 # Remove the lock file and exit
 rm -f "$SYNC_LOCK"
