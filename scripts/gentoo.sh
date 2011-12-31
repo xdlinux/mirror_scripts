e5d003b80102f168009f8998e7d8ff4abfde8e58
xdlinux <xdl@xdlinux.info>
2011-12-31 22:36:37 +0800

    gentoo/ubuntu status enable

--- scripts/gentoo.sh
+++ scripts/gentoo.sh
@@ -8,7 +8,8 @@
 # Licensed under the GNU GPL (version 2)
 
 # Filesystem locations for the sync operations
-SYNC_HOME="/home/bigeagle/mirror"
+source `dirname $0`/functions.d/functions
+
 SYNC_LOGS="$SYNC_HOME/logs/gentoo"
 SYNC_FILES="/srv/ftp/gentoo"
 SYNC_LOCK="$SYNC_HOME/gentoo.lck"
@@ -18,6 +19,9 @@ SYNC_LOCK="$SYNC_HOME/gentoo.lck"
 SYNC_SERVER="rsync://mirrors.xmu6.edu.cn/gentoo/"
 LOG_FILE="gentoo_$(date +%Y%m%d-%H).log"
 
+STAT_FILE="$SYNC_HOME/status/gentoo"
+
+set_stat $STAT_FILE "-1"
 # Do not edit the following lines, they protect the sync from running more than
 # one instance at a time
 if [ ! -d $SYNC_HOME ]; then
@@ -36,8 +40,12 @@ echo ">> Starting sync on $(date --rfc-3339=seconds)" >> "$SYNC_LOGS/$LOG_FILE"
 echo ">> ---" >> "$SYNC_LOGS/$LOG_FILE"
 #starting rsync
 
+
 rsync -6 -av --delete-after --exclude *.~tmp~*  $SYNC_SERVER $SYNC_FILES >> $SYNC_LOGS/$LOG_FILE
 
+set_stat $STAT_FILE $?
+
+date --rfc-3339=seconds > "$SYNC_FILES/lastsync"
 # Insert another timestamp and close the log file
 echo ">> ---" >> "$SYNC_LOGS/$LOG_FILE"
 echo ">> Finished sync on $(date --rfc-3339=seconds)" >> "$SYNC_LOGS/$LOG_FILE"
--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -8,7 +8,8 @@
 # Licensed under the GNU GPL (version 2)
 
 # Filesystem locations for the sync operations
-SYNC_HOME="/home/bigeagle/mirror"
+source `dirname $0`/functions.d/functions
+
 SYNC_LOGS="$SYNC_HOME/logs/ubuntu"
 SYNC_FILES="/srv/ftp/ubuntu"
 SYNC_LOCK="$SYNC_HOME/ubuntu.lck"
@@ -18,6 +19,7 @@ SYNC_SERVER=rsync://mirrors6.ustc.edu.cn/ubuntu
 #SYNC_SERVER=rsync://mirrors.xmu6.edu.cn/ubuntu-archive
 LOG_FILE="ubuntu_$(date +%Y%m%d-%H).log"
 
+STAT_FILE="$SYNC_HOME/status/ubuntu"
 # Do not edit the following lines, they protect the sync from running more than
 # one instance at a time
 if [ ! -d $SYNC_HOME ]; then
@@ -28,6 +30,8 @@ fi
 [ -f $SYNC_LOCK ] && exit 1
 touch "$SYNC_LOCK"
 # End of non-editable lines
+
+set_stat $STAT_FILE "-1"
 
 # Create the log file and insert a timestamp
 touch "$SYNC_LOGS/$LOG_FILE"
@@ -45,6 +49,9 @@ rsync -6 -av --delete-after \
 --exclude *.orig.tar.gz --exclude *.diff.gz \
 --exclude *.dsc \
 $SYNC_SERVER $SYNC_FILES >> $SYNC_LOGS/$LOG_FILE
+
+set_stat $STAT_FILE $?
+date --rfc-3339=seconds > "$SYNC_FILES/lastsync"
 
 # Insert another timestamp and close the log file
 echo ">> ---" >> "$SYNC_LOGS/$LOG_FILE"
