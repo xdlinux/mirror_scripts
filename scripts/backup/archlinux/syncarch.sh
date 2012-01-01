011b1cc2830b90056307608fba0e5f6822d1c35d
bigeagle <justin.w.xd@gmail.com>
2012-01-01 17:17:16 +0800

    清理代码

--- scripts/backup/archlinux/syncarch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#/home/mirror/scripts/archlinux/mirrorsync_comm.sh &
-#/home/mirror/scripts/archlinux/mirrorsync_core.sh & 
-#/home/mirror/scripts/archlinux/mirrorsync_extra.sh &
-/home/bigeagle/mirror/scripts/archlinux/mirrorsync.sh &
-/home/bigeagle/mirror/scripts/archlinux/pool.sh &
-
