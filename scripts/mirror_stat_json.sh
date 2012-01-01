07172e69f20741e43245e6792ecc1db1eec7fa1a
bigeagle <justin.w.xd@gmail.com>
2012-01-01 20:08:51 +0800

    status2json and status2tweet

--- scripts/mirror_stat_json.sh
+++ scripts/mirror_stat_json.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-STAT_DIR="/home/work/mirror/status"
+source  `dirname $0`/functions.d/functions
+STAT_DIR="$SYNC_HOME/status"
+LOG_DIR="$SYNC_HOME/logs"
 JSON="/srv/www/m_status.json"
+SRV_ROOT="/srv/www"
+LOG_URL="/index/mlog"
 LOCK="/tmp/mirror_json.lck"
 
 lockfile-create $LOCK
@@ -9,6 +13,9 @@ lockid="$!"
 
 echo "{" > $JSON
 for REPO in `ls $STAT_DIR`;do
+    LOG=`ls -t $LOG_DIR/$REPO|head -1` 
+    cp "$LOG_DIR/$REPO/$LOG" "$SRV_ROOT$LOG_URL/$REPO.log"
+    set_stat $STAT_DIR/$REPO "log" "$LOG_URL/$REPO.log"
 	STAT=`cat $STAT_DIR/$REPO`
 	echo "	\"$REPO\":$STAT," >> $JSON
 done
